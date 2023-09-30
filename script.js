931aa5dbfd4991f82a5f166b17c2829423ea3416
Jesse russo <jesserusso90@gmail.com>
2023-09-29 22:42:41 -0700

    adds playerFactory, makeMove, and clearBoard

--- script.js
+++ script.js
@@ -4,34 +4,51 @@ const gameBoard = (() => {
 
     const createBoard= () => {
         for (let i = 0; i < 9; i++){
-            let cell = document.createElement('div');
-            cell.classList.add('game-cell');
-            cell.id = `Cell${i}`;
-            gameDisplay.appendChild(cell);
+            //let cell = document.createElement('div');
+            //cell.classList.add('game-cell');
+            //cell.id = `Cell${i}`;
+            //gameDisplay.appendChild(cell);
             //TEMP: adds filler cell values
-            board.push({mark: 'x', position: i});
+            //board.push({player: {name: 'jesse', marker: 'x'}, position: i});
         };
     }
     const addToBoard = (player, location) => {
-        board.push({player, location});
-        return board;
+        board[location] = ({player});
     };
+    const clearBoard = () => {
+        board.length = 0;
+        for (let i = 0; i < 9; i++){
+            const cell = document.getElementById(`${i}`)
+            cell.innerText = '';
+        }
+    }
     const displayBoard = () => {
         board.forEach((e) => {
-            const cell = document.getElementById(`Cell${board.indexOf(e)}`);
-            cell.innerText = e.mark;
+            const cell = document.getElementById(`${board.indexOf(e)}`);
+                if (e.player.marker != undefined){
+                    cell.innerText = e.player.marker;
+                }
         })
     };
-
-    return {addToBoard, createBoard, gameDisplay, displayBoard};
+    return {addToBoard, createBoard, gameDisplay, displayBoard, board, clearBoard};
 })();
 
 const controller = (() => {
     const players = [];
+    const currentPlayer = null;
     let gameDisplay = document.querySelector('game-container');
 
-    return {players};
+    const addPlayer = (name, marker) => {
+        if (players.length < 2){
+            players.push(playerFactory(name, marker))
+        }
+    }
+    const makeMove = (player, cell) => {
+        gameBoard.addToBoard(player, cell)
+        gameBoard.displayBoard();
+    }
+    return {players, addPlayer, makeMove};
 })();
-const playerFactory = () => {
-
+const playerFactory = (name, marker) => {
+    return {name, marker};
 };
